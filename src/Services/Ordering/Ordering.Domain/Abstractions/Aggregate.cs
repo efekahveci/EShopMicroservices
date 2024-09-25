c17b672f478a0867aa64c218dd63a146190547f0
Efe Kahveci <efekhvci@hotmail.com>
2024-09-25 20:21:54 +0300

    Add Ordering service projects and update solution
    Updated `Ordering.API.csproj` to include new folders and project references for `Ordering.Application` and `Ordering.Infrastructure`. Modified `Program.cs` to add new using directives and services. Updated `launchSettings.json` to modify the `launchUrl` setting. Updated `eshop-microservices.sln` to include new project references.
    
    Added `DependencyInjection.cs` and `GlobalUsing.cs` files to `Ordering.API`, `Ordering.Application`, and `Ordering.Infrastructure` namespaces. Created `Ordering.Application.csproj` and `Ordering.Domain.csproj` with necessary references and folders. Added domain abstraction files to `Ordering.Domain`. Created `Ordering.Infrastructure.csproj` with a reference to `Ordering.Application`.

---
+++ src/Services/Ordering/Ordering.Domain/Abstractions/Aggregate.cs
@@ -0,0 +1,16 @@
+﻿namespace Ordering.Domain.Abstractions;
+public abstract class Aggregate<TId> : Entity<TId>, IAggregate<TId>
+{
+    private readonly List<IDomainEvent> _domainEvents = [];
+    public IReadOnlyList<IDomainEvent> DomainEvents => _domainEvents.AsReadOnly();
+    public IDomainEvent[] ClearDomainEvents()
+    {
+        var events = _domainEvents.ToArray();
+        _domainEvents.Clear();
+        return events;
+    }
+    protected void AddDomainEvent(IDomainEvent domainEvent)
+    {
+        _domainEvents.Add(domainEvent);
+    }
+}
